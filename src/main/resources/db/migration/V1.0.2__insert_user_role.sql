6587a3135faaff8defe47b51e288b1b8285c4d6b
ronalds.norvezs <ronalds.norvezs@accenture.com>
2022-12-09 11:33:41 +0200

    commit

--- src/main/resources/db/migration/V1.0.2__insert_user_role.sql
+++ src/main/resources/db/migration/V1.0.2__insert_user_role.sql
@@ -1,6 +1,6 @@
 use eventdb;
 
-INSERT INTO user_role (role_id, role)
+INSERT INTO role (role_id, role)
 VALUE ('1', 'admin');
-INSERT INTO user_role (role_id, role)
+INSERT INTO role (role_id, role)
 VALUE ('2', 'user');
--- src/main/resources/db/migration/V1.0.4__insert_event_type.sql
+++ src/main/resources/db/migration/V1.0.4__insert_event_type.sql
@@ -1,6 +1,6 @@
 use event_db;
 
-INSERT INTO event_type (type_id, type)
+INSERT INTO type (type_id, type)
 VALUE ('1', 'public');
-INSERT INTO event_type (type_id, type)
+INSERT INTO type (type_id, type)
 VALUE ('2', 'private');
