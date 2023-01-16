0216f87c41c19b3315c548986d2f2d52a5868b41
ronalds.norvezs <ronalds.norvezs@accenture.com>
2023-01-16 10:42:40 +0200

    .

--- src/main/java/eventservice/eventservice/business/repository/EventRepository.java
+++ src/main/java/eventservice/eventservice/business/repository/EventRepository.java
@@ -15,10 +15,14 @@ import java.util.List;
 @Repository
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
     List<EventEntity> findAllByCountryAndTypeType(String country, String type);
-
     List<EventEntity> findAllByCountryAndTypeTypeAndCity(String country, String type, String city);
-
     List<EventEntity> findAllByCountryAndTypeTypeAndCityAndDateTimeBetween(String country, String type, String city, LocalDateTime dateFrom, LocalDateTime dateTo);
-
     List<EventEntity> findAllByCountryAndTypeTypeAndDateTimeBetween(String country, String type, LocalDateTime dateFrom, LocalDateTime dateTo);
+
+
+    List<EventEntity> findAllByCountry(String country);
+    List<EventEntity> findAllByCountryAndCity(String country, String city);
+    List<EventEntity> findAllByCountryAndCityAndDateTimeBetween(String country, String city, LocalDateTime dateFrom, LocalDateTime dateTo);
+    List<EventEntity> findAllByCountryAndDateTimeBetween(String country, LocalDateTime dateFrom, LocalDateTime dateTo);
+
 }
