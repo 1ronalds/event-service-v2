669d1993f3edbe9f96d7c01ef2ede6f098725169
martynas.slavinskas <martynas.slavinskas@accenture.com>
2023-01-24 13:15:50 +0200

    added configurable property for feign client name

--- src/main/java/eventservice/eventservice/business/connection/CountryCityServiceConnection.java
+++ src/main/java/eventservice/eventservice/business/connection/CountryCityServiceConnection.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "countrycityservice", url = "${feignclient.url}")
+@FeignClient(name = "${feignclient.name}", url = "${feignclient.url}")
 public interface CountryCityServiceConnection {
 
     @GetMapping("/v1/countries")
